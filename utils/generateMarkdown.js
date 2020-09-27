a794618465c7362bb46da0c948ade257538e7132
Owais Islam <owaisislam@gmail.com>
2020-09-26 19:20:07 -0500

    updated README to include more sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,6 +3,21 @@ function generateMarkdown(data) {
   return `# ${data.title}
 ## Description
 ${data.description}
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Contributing
+${data.contribution}
+## Tests
+${data.test}
 `;
 }
 
